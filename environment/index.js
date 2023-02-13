a9392c8ca08ed6933c47cb61197d2e81e6a03032
Erfan Yousefi <erfanyousefi.co@gmail.com>
2023-02-14 00:28:25 +0330

    environment tutorials

--- environment/index.js
+++ environment/index.js
@@ -1,29 +1,17 @@
 const express = require("express");
-const cookieParser = require("cookie-parser")
 const app = express();
-app.use(cookieParser("20fbbf3cac627dd620d7a874e4711c50fd5499eb"))
-app.get("/set-cookie", (req, res) => {
-    const d = new Date()
-    res.cookie('python', "django", {
-        maxAge: 50000,
-        expires: new Date(d.getTime() + 50000),
-        httpOnly: true,
-        signed: true,
-        secure: true,
-        sameSite: "lax" //lax - strict - none
-    });
-    res.cookie('nodejs', 'javascript, Typescript')
-    res.send("cookie have been saved successfully")
+const dotenv = require("dotenv");
+const path = require("path");
+dotenv.config()
+const NodeEnv = process.env.NODE_ENV;
+dotenv.config({
+    path: path.join(__dirname, `.env.${NodeEnv}`)
 })
-app.get("/get-cookie", (req, res) => {
-    const cookies = req.cookies;
-    const signedCookies = req.signedCookies
-    res.send({cookies, signedCookies})
+require("./blog")
+app.get("/", (req, res,next) => {
+    console.log(process.env.API_KEY);
+    res.send("server run")
 })
-app.get("/clear-cookie/:name", (req, res) => {
-    res.clearCookie(req.params.name)
-    res.send("Cookie has been deleted successfully")
-})
-app.listen(3000, () => {
-    console.log("server run on port 3000");
+app.listen(process.env.PORT, () => {
+    console.log("server run on port " + process.env.PORT);
 })
